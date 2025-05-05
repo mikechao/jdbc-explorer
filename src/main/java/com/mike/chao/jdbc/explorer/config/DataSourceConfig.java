488cdcbbf05c8d3c16c7e570e54bba714ce87564
mikechao <mike.chao.one@gmail.com>
2025-05-05 11:51:30 -0700

    Clean up imports

--- src/main/java/com/mike/chao/jdbc/explorer/config/DataSourceConfig.java
+++ src/main/java/com/mike/chao/jdbc/explorer/config/DataSourceConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Configuration
 public class DataSourceConfig {
 
